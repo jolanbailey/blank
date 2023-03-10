ce6b6a4a858452cf60e7e2f7237e2c116e2c9e18
Jolan Bailey <jolanbailey@gmail.com>
2023-03-10 22:57:05 +0000

    Added additional config and a session secret

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < Sinatra::Base
-    #configure do
-    #    set :public_folder, 'public'
-    #    set :views, 'app/views' enable :sessions
-    #    set :session_secret, 'password_security'
-    #end
+    configure do
+        set :public_folder, 'public'
+        set :views, 'app/views' 
+        enable :sessions
+        set :session_secret, 'td2ggb+SRaOk3Kt23/Q6XVTYFtDK57sTUcs7SgM0a6+mmq1JO9+9vnleTEAiL0ko'
+    end
     
     get '/' do
         File.read(File.join('app/views', 'index.html'))
